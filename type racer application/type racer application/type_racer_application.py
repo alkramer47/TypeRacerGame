2335e7f38698fdc223531faab89cca383562906b
alkramer47 <43616972+alkramer47@users.noreply.github.com>
2021-03-02 01:38:29 -0600

    Add files via upload

--- type racer application/type racer application/type_racer_application.py
+++ type racer application/type racer application/type_racer_application.py
@@ -2,20 +2,21 @@ from tkinter import *
 from timeit import default_timer as timer
 import random
 from time import sleep
+import time
 
-#TODO :: reconfigure check_result() to display the accuracy of the user-typed string 
-#TODO :: Create a countdown for the timer, stop the timer after hitting submit, and show the user their results
 #TODO :: Add fun pictures and work on the GUI
-#TODO :: Fix the .after() function countdown
 #TODO :: open the window allowing user to read code, start 5 second timer, then allow user to type after 5 seconds
-#TODO :: think about using text instead of entry for text box
 #TODO :: make an easy, medium, and hard mode
-#TODO :: calculate net WPM after figuring out how to calculate mistakes
-#TODO :: stop the timer when user is done
-#TODO :: make it so that when there is more or less characters in the user_quote that it tells the user to try again and that there is insufficient data to determine WPM (or fix it, whatever)
+#TODO :: take every word in quote and put it into a list seperated by spaces, do the same for the user's input, and compare each element for mistakes, increment mistakes
+
 
 x=0
 y=0
+
+
+
+def results(event): #TODO :: add all of the results to this function, clear the screen after user is done typing and lead to this screen showing results
+    add_stuff=0
 
 def game(event): #fix the event warning on console when manual enter is pressed
     global x
@@ -34,7 +35,7 @@ def game(event): #fix the event warning on console when manual enter is pressed
 
     if y==0: #FIXME :: countdown before the game starts to users can see the quote before they type it
         def countdown(self):
-            self.sleep(10)
+            main.after(self.sleep(10))
             print("test")
 
     def check_result(event): #checks if word is spelt correctly
@@ -66,20 +67,22 @@ def game(event): #fix the event warning on console when manual enter is pressed
         #time_seconds = ("%.2f" % time_seconds)
 
         num_char=0
-        for i in range(len(user_quote)):
+        for i in range(len(user_quote)): #counts the number of characters the user types
             num_char += 1
-        wpm = ((num_char/5)-mistakes)/minutes_and_seconds
+        wpm = ((num_char/5)-mistakes)/minutes_and_seconds #calculation for wpm
         print("Words per min:", wpm)
         wpm = ("%.2f" % wpm)
-        #wpm_string=str(wpm)
-        wpm_label=Label(window,text=wpm,font='times 20')
-        wpm_label.place(x=700,y=50)
+        wpm_string=str(wpm)
+        wpm_results = wpm + " WPM" 
+        wpm_label=Label(window,text=wpm_results,font='times 20') #wpm results label
+        wpm_label.place(x=240,y=270)
 
-        print(minutes_and_seconds)
+        print(minutes_and_seconds) #tests
         print(time_minutes)
         print(time_seconds)
 
         time_minutes_string = str(time_minutes) #converts the minutes, seconds, and num_words to strings to fit into Label 
+        time_seconds = '{0:.2g}'.format(time_seconds)
         time_seconds_string = str(time_seconds)
         num_words_string = str(num_words)
 
@@ -92,42 +95,42 @@ def game(event): #fix the event warning on console when manual enter is pressed
         check_message=Label(window,text=final_result,font="times 18")  #label that show the amount of time used to type the quote
         check_message.place(x=25,y=450)
 
-    with open('TextFile1.txt', 'r') as f: #opens the text file to create a list out of the quotes
+    with open('quotes.txt', 'r') as f: #opens the text file to create a list out of the quotes
         lines = f.read().splitlines()
 
     quote = random.randint(0,len(lines)-2) #grabs a random quote and displays it
-    x2=Message(window,text = lines[quote],font="times 15") 
-    x2.place(x=300,y=100)
-    #x2.pack(ipadx=300,ipady=300)
+    x2=Message(window,text = lines[quote],pady=100,font="times 15") 
+    x2.pack(fill=X)
+    x2.place(x=115,y=0)
 
     num_words = len(lines[quote].split()) #counts the words in the quote
     print(num_words)
 
-    entry = Entry(width=50) #entry line that allows user to type the quote
-    entry.place(x=150,y=55)
+    entry = Entry(width=70) #entry line that allows user to type the quote
+    entry.place(x=115,y=50)
     entry.focus()
 
-    c1= Label(window,text="Begin!!",font='times 20') #begin label 
-    c1.place(x=10,y=10)
+    #c1= Label(window,text="Begin!",font='times 20') #begin label 
+    #c1.place(x=10,y=10)
 
-    b2=Button(window,text="Submit",command=check_result,width=12,bg='red') #submit button binded to return key
+    b2=Button(window,text="Press enter when finished",command=check_result,width=20,bg='red') #submit button binded to return key
     window.bind('<Return>', check_result)
-    b2.place(x=150,y=100)
+    b2.place(x=240,y=225)
 
     start=timer() #timer that counts the time it takes to type quote
 
 #main window
 window = Tk() 
-window.geometry("900x500")
+window.geometry("700x400")
 window.title("Adam's typing speed test")
 window.attributes('-topmost',True)
 #window.lift()
 
 #start screen
-x1=Label(window,text = "Welcome to the typing speed test. Let's begin...", font = "times 12")
-x1.place(x=315,y=200)
+x1=Label(window,text = "Welcome to the typing speed test. Let's begin...", font = "times 14")
+x1.place(x=165,y=140)
 b1 = Button(window,text="Press Enter",command=game,width=12,bg='red')
-b1.place(x=400,y=250)
+b1.place(x=280,y=180)
 window.bind('<Return>', game)
 
 #run the GUI
